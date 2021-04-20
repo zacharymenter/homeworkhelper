2bb29adc247f07a783f745227ff44f3e35c943cf
Alexis Lappe <LAPPEAA0646@UWEC.EDU>
2021-04-20 17:47:10 -0500

    PHP Login Code

---
+++ insert_student.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+    <center>
+        <?php
+  
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        // Get a connection for the database
+        require_once('sql_conn.php');
+          
+       
+          
+        // Taking all 5 values from the form data(input)
+        $email =  $_REQUEST['email'];
+        $name = $_REQUEST['name'];
+        $username = $_REQUEST['username']; 
+        $password =  $_REQUEST['password'];
+        $school = $_REQUEST['schools']; 
+
+          
+        // Performing insert query execution
+        // here our table name is college
+        $query = "INSERT INTO student (S_name, email, username, s_password, school) VALUES ('$name', 
+            '$email','$username', '$password', '$school')";
+          
+        // Get a response from the database by sending the connection
+        // and the query
+        $response = @mysqli_query($dbc, $query);
+
+          
+        // Close connection
+        mysqli_close($dbc);
+        header('Location: two_factor.html'); 
+        ?>
+    </center>
+
+
+
+
+</body>
+  
+</html>
