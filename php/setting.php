0397d3428b83951fabba72f1d067e230e7d91e4f
baraneha5997 <baraneha5997@uwec.edu>
2021-04-27 00:38:20 -0500

    google update
    another update tomorrow

---
+++ php/setting.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '755526323013-vrhpdhmml0gf0c5pve3cushqe4pnl52b.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'J8fGNXRgyHD-kyWsOsqxEd4i');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/homeworkhelper/index.php');
+
+?>
